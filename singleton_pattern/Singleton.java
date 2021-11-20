f1a078ca99efd74401508553ccfe285b010492db
Md. Neyamul Islam <68305701+mdneyamul@users.noreply.github.com>
2021-11-20 13:44:18 +0600

    Add files via upload

---
+++ singleton_pattern/Singleton.java
@@ -0,0 +1,12 @@
+public class Singleton{
+    static private Singleton instance;
+    private Singleton(){
+
+    }
+    public static synchronized Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
